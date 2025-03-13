db27da2da45e72184fb14181fece7291fca76bd0
hxddad <haddad.yazan117@gmail.com>
2025-03-13 12:18:31 -0400

    commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,12 +12,12 @@ const config: Config = {
   url: 'https://hxddad.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/york-swe-edge/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'hxddad', // Usually your GitHub org/user name.
-  projectName: 'hxddad.github.io', // Usually your repo name.
+  projectName: 'york-swe-edge', // Usually your repo name.
   trailingSlash: false,
   
   onBrokenLinks: 'throw',
