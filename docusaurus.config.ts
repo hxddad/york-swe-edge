98a60edc2caac11dc3db6e12ed732f631fc62a8f
hxddad <haddad.yazan117@gmail.com>
2025-05-03 11:57:47 -0400

    added gt mngr id

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,9 +58,9 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
-        // googleTagManager: {
-        //   containerId: process.env.CONTAINER_ID
-        // },
+        googleTagManager: {
+          containerId: 'GTM-T9ZVMPLX',
+        },
         // gtag: {
         //   trackingID: process.env.GTAG_ID,  
         //   anonymizeIP: true, 
